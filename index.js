d82227fd073d347033e464f76ba1498e178bb8e8
Trevor Blackman <TJBlackman08@gmail.com>
2018-12-10 20:24:08 -0700

    Try catch + whitelisting
    I added a try catch block to prevent errors from killing the function. I also added an origin check to only allow requests from bradshousermk.com

--- index.js
+++ index.js
@@ -5,33 +5,46 @@ const querystring = require('querystring');
 // setup sendgrid
 sgMail.setApiKey(config.SG_API_KEY);
 
-module.exports = async req => {
-  
-  const values = querystring.parse(req.url)
-  console.log('=====!!!===== New Form Submission =====!!!=====');
-  console.log(req.url);
+module.exports = async (req, res) => {
+  try {
+    // only accept requests from bradshousermk.com
+    if (!req.headers.origin.includes('bradshousermk.com')){
+      res.end('Unauthorized.');
+      return; 
+    }
 
-  const html = Object.entries(values).reduce((acc, item) => {
-    return `${acc}<p>${item[0].replace('/?','')}: ${item[1]}</p>`;
-  },'');
+    console.log('=====!!!===== New Form Submission =====!!!=====');
+    const values = querystring.parse(req.url)
+    console.log(req.url);
 
-  const email = {
-    to: config.TO,
-    from: config.FROM,
-    bcc: config.BCC,
-    subject: config.SUBJECT,
-    html: html
-  }; 
+    const html = Object.entries(values).reduce((acc, item) => {
+      return `${acc}<p>${item[0].replace('/?','')}: ${item[1]}</p>`;
+    },'');
+    console.log(html);
 
-  const replyTo = values['Email Address'];
-  if (replyTo){
-    email.replyTo = values['Email Address'];
-  } else {
-    console.log('Error: Unable to assign replyTo address.');
-    console.log(`Error: values['Email Address'] returns "${replyTo}".`);
+    const email = {
+      to: config.TO,
+      from: config.FROM,
+      bcc: config.BCC,
+      subject: config.SUBJECT,
+      html: html
+    }; 
+
+    const replyTo = values['Email Address'];
+    if (replyTo){
+      email.replyTo = values['Email Address'];
+    } else {
+      console.log('Error: Unable to assign replyTo address.');
+      console.log(`Error: values['Email Address'] returns "${replyTo}".`);
+      return;
+    }
+
+    sgMail.send(email, () => {
+      console.log(`Form sent successfully. ${new Date()}`);
+      res.end(`Form sent successfully. ${new Date()}`);
+    });
+
+  } catch (err){
+    console.log(err);
   }
-
-  sgMail.send(email, () => {
-    console.log(`Form sent successfully. ${new Date()}`);
-  });
 }
