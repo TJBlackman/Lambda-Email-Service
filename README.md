666ce1d0e9edb209b244ee47c1eeb847289847c5
TJBlackman <TJBlackman08@gmail.com>
2020-09-20 08:34:10 -0600

    new email service

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Lambda-Email-Service
 
-A serverless function that receives form data and sends an email using the SendGrid email API. Uses some very basic domain whitelisting to prevent spam. Service is hosted with Zeit hosting.
+A serverless function that receives form data and sends an email using the SendGrid email API. Uses some very basic domain whitelisting to prevent spam. Service is hosted with AWS hosting.
+
+#### Example request
+
+```js
+fetch('/api/email', {
+    method: 'POST',
+    body: {
+        name: 'john doe',
+        subject: 'Domain.com | Contact Form'
+        emailAddress: 'john.doe@gmail.com',
+        message: 'I would like to get in touch with you!'
+    }
+});
+```
--- index.js
+++ index.js
@@ -1,50 +1,85 @@
-const config = require('./config');
+require('dotenv').config();
+
+const express = require('express');
 const sgMail = require('@sendgrid/mail');
-const querystring = require('querystring');
+const app = express();
+const port = 3000;
 
-// setup sendgrid
-sgMail.setApiKey(config.SG_API_KEY);
+// create html email
+const makeHtmlEmail = (site, body) => {
+  return `
+    <p>New email from ${site.domain}!
+      <br/>
+      <b>Do NOT reply to this email.</b>
+    </p>
+    <p>
+      <span>Name: </span>
+      <span>${body.name}</span>
+    </p>
+    <p>
+      <span>Email Address: </span>
+      <span>${body.emailAddress}</span>
+    </p>
+    <p>
+      <span>Message:</span>
+      <br/>
+      <span>${body.message}</span>
+    </p>
+  `;
+};
 
-module.exports = async (req, res) => {
+// sg api setup
+sgMail.setApiKey(process.env.SG_API_KEY);
+
+// whitelist websites
+const websites = {
+  'bradshousermk.com': {
+    domain: 'bradshousermk.com',
+    to: 'nick@gmail.com',
+    from: 'website@bradshousermk.com',
+    bcc: 'TJBlackman08@gmail.com',
+  },
+  'trevorblackman.io': {
+    domain: 'trevorblackman.io',
+    to: 'TJBlackman08@gmail.com',
+    from: 'website@trevorblackman.io',
+  },
+  localhost: {
+    domain: 'localhost',
+    to: 'tjblackman08@gmail.com',
+    from: 'localhost@bradshouse.rmk',
+  },
+};
+
+// echo controller
+app.get('/api/echo/:msg', (req, res) => {
+  res.send(`${new Date().toLocaleString()} - You wrote: "${req.params.msg}"`);
+});
+
+// mail controller
+app.post(`/api/email`, async (req, res) => {
   try {
-    // only accept requests from bradshousermk.com
-    if (!req.headers.origin.includes('bradshousermk.com')){
-      res.end('Unauthorized.');
-      return; 
+    // affirm request is from whitelist domain
+    const site = websites[req.hostname];
+    if (!site) {
+      throw Error('Origin not approved.');
     }
 
-    console.log('=====!!!===== New Form Submission =====!!!=====');
-    const values = querystring.parse(req.url)
-    console.log(req.url);
-
-    const html = Object.entries(values).reduce((acc, item) => {
-      return `${acc}<p>${item[0].replace('/?','')}: ${item[1]}</p>`;
-    },'');
-    console.log(html);
-
-    const email = {
-      to: config.TO,
-      from: config.FROM,
-      bcc: config.BCC,
-      subject: config.SUBJECT,
-      html: html
-    }; 
-
-    const replyTo = values['Email Address'];
-    if (replyTo){
-      email.replyTo = values['Email Address'];
-    } else {
-      console.log('Error: Unable to assign replyTo address.');
-      console.log(`Error: values['Email Address'] returns "${replyTo}".`);
-      return;
-    }
-
-    sgMail.send(email, () => {
-      console.log(`Form sent successfully. ${new Date()}`);
-      res.end(`Form sent successfully. ${new Date()}`);
+    const msg = {
+      to: site.to,
+      from: site.from,
+      subject: req.body.subject,
+      text: req.body.subject,
+      html: makeHtmlEmail(site, req.body),
+    };
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
     });
-
-  } catch (err){
-    console.log(err);
   }
-}
+});
+
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
